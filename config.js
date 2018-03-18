c01193ea8336ca83e259f6c2e6b195b9afa013da
Joanna Krężołek <j.krezolek@gmail.com>
2018-03-18 11:21:51 +0100

    expressJS and Google API

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '573543797705-oo8ogfctabtuoiqd0nh7bt13bolkfj98.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'WAataMjBlKNkIj6C_9_8a1Cm',
+	'CALLBACK_URL': 'http://localhost:3001/auth/google/callback'
+};
